4b24bf835e1b88d4ce50975225d1f8f5887b4403
Roy <codefighter@foxmail.com>
2017-10-18 15:00:01 +0800

    添加了自定义命令类，可以支持MVVM模式部分实现

---
+++ WpfUsefulCode/WpfExtended/MVVM/CustomCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input; 
+
+namespace WpfExtended.MVVM
+{
+    public class RelayCommand:ICommand
+    {
+        private readonly Action     _executeMethod = null;
+        private readonly Func<bool> _canExecuteMethod = null;
+        private bool                _isAutomaticRequeryDisabled = false;
+
+        public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod, bool isAutomaticRequeryDisabled)
+        {
+            if(executeMethod ==null)
+            {
+                throw new ArgumentNullException("ExecuteMethod is not set.");
+            }
+            _executeMethod              = executeMethod;
+            _canExecuteMethod           = canExecuteMethod;
+            _isAutomaticRequeryDisabled = isAutomaticRequeryDisabled;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if(!_isAutomaticRequeryDisabled)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if(!_isAutomaticRequeryDisabled)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if(_canExecuteMethod != null)
+            {
+                return _canExecuteMethod();
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod?.Invoke();
+        }
+    }
+}
---
+++ WpfUsefulCode/WpfExtended/MVVM/RelayCommandGeneric.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfExtended.MVVM
+{
+    public class RelayCommandGeneric<T> : ICommand
+    {
+        private readonly Action<T>    _executeMethod    = null;
+        private readonly Func<T,bool> _canExecuteMethod = null;
+        private bool                  _isAutomaticRequeryDisabled = false;
+
+        public RelayCommandGeneric(Action<T> executeMethod, Func<T,bool> canExecuteMethod, bool isAutomaticRequeryDisabled)
+        {
+            if(executeMethod ==null)
+            {
+                throw new ArgumentNullException();
+            }
+            _executeMethod              = executeMethod;
+            _canExecuteMethod           = canExecuteMethod;
+            _isAutomaticRequeryDisabled = isAutomaticRequeryDisabled;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if(!_isAutomaticRequeryDisabled)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if(!_isAutomaticRequeryDisabled)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if(_canExecuteMethod != null && parameter is T)
+            {
+                _canExecuteMethod((T)parameter);
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(_executeMethod != null && parameter is T)
+            {
+                _executeMethod((T)parameter);                   
+            }
+        }
+    }
+}
